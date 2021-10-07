ac314fe44686e4c62d2885dad68f06f7fc52e2b6
Leire <lvilmar1@epsg.upv.es>
2021-10-07 21:30:20 +0200

    Pojo

---
+++ app/src/main/java/org/example/lvilmar1/leireapp/logicaFake/Medicion.java
@@ -0,0 +1,25 @@
+package org.example.lvilmar1.leireapp.logicaFake;
+
+public class Medicion {
+    private int medicion;
+    public double latitud;
+    public double longitud;
+
+    public Medicion(int medicion) {
+        this.medicion = medicion;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public int getMedicion() {
+        return medicion;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+}
